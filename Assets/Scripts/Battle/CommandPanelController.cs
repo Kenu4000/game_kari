b32746aa68e8d2b527c700ace75b2ab860fbb386
Kenu4000 <komabuchi4000@gmail.com>
2026-05-12 13:14:16 +0900

    Adjust CommandPanelController initialization flow

--- Assets/Scripts/Battle/CommandPanelController.cs
+++ Assets/Scripts/Battle/CommandPanelController.cs
@@ -1,129 +1,110 @@
-using System;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace GameKari.Battle
 {
-    public enum CommandViewMode
-    {
-        Skills,
-        Swap,
-        Items
-    }
-
     public class CommandPanelController : MonoBehaviour
     {
-        [Header("Root Buttons")]
+        [Header("Panels")]
+        [SerializeField] private GameObject mainCommandButtons;
+        [SerializeField] private GameObject skillListPanel;
+        [SerializeField] private GameObject swapListPanel;
+        [SerializeField] private GameObject itemListPanel;
+
+        [Header("Buttons")]
         [SerializeField] private Button fightButton;
         [SerializeField] private Button swapButton;
         [SerializeField] private Button itemButton;
 
-        [Header("Runtime Content")]
-        [SerializeField] private Transform skillListRoot;
-        [SerializeField] private Transform sideListRoot;
-        [SerializeField] private Button simpleButtonPrefab;
-        [SerializeField] private TMP_Text descriptionText;
-
-        public Action<SkillData> OnSkillClicked;
-        public Action<SkillData> OnSkillHovered;
-        public Action OnHoverExit;
-        public Action<BattleUnit> OnReserveClicked;
-        public Action<string> OnItemClicked;
-
-        private BattleUnit _activeUnit;
-        private List<BattleUnit> _reserves;
-        private CommandViewMode _mode;
-
-        public void Setup(BattleUnit activeUnit, List<BattleUnit> reserves)
+        private void Awake()
         {
-            _activeUnit = activeUnit;
-            _reserves = reserves;
-            HookRootButtons();
-            OpenSkills();
+            BindButtons();
         }
 
-        private void HookRootButtons()
+        private void Start()
         {
-            fightButton.onClick.RemoveAllListeners();
-            swapButton.onClick.RemoveAllListeners();
-            itemButton.onClick.RemoveAllListeners();
-
-            fightButton.onClick.AddListener(OpenSkills);
-            swapButton.onClick.AddListener(OpenSwap);
-            itemButton.onClick.AddListener(OpenItems);
+            ShowMain();
         }
 
-        public void OpenSkills()
+        private void OnDestroy()
         {
-            _mode = CommandViewMode.Skills;
-            ClearChildren(skillListRoot);
-            ClearChildren(sideListRoot);
+            UnbindButtons();
+        }
 
-            foreach (SkillData skill in _activeUnit.Skills)
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
             {
-                Button btn = Instantiate(simpleButtonPrefab, skillListRoot);
-                btn.GetComponentInChildren<TMP_Text>().text = skill.SkillName;
-                btn.onClick.AddListener(() => OnSkillClicked?.Invoke(skill));
-
-                EventTrigger trigger = btn.gameObject.AddComponent<EventTrigger>();
-                AddHoverEvent(trigger, EventTriggerType.PointerEnter, () =>
-                {
-                    descriptionText.text = skill.Description;
-                    OnSkillHovered?.Invoke(skill);
-                });
-                AddHoverEvent(trigger, EventTriggerType.PointerExit, () => OnHoverExit?.Invoke());
+                ShowMain();
             }
         }
 
-        private void OpenSwap()
+        public void ShowMain()
         {
-            _mode = CommandViewMode.Swap;
-            ClearChildren(skillListRoot);
-            ClearChildren(sideListRoot);
+            SetActive(mainCommandButtons, true);
+            SetActive(skillListPanel, false);
+            SetActive(swapListPanel, false);
+            SetActive(itemListPanel, false);
+        }
 
-            foreach (BattleUnit reserve in _reserves)
+        public void ShowSkills()
+        {
+            SetActive(mainCommandButtons, true);
+            SetActive(skillListPanel, true);
+            SetActive(swapListPanel, false);
+            SetActive(itemListPanel, false);
+        }
+
+        public void ShowSwap()
+        {
+            SetActive(mainCommandButtons, true);
+            SetActive(skillListPanel, false);
+            SetActive(swapListPanel, true);
+            SetActive(itemListPanel, false);
+        }
+
+        public void ShowItems()
+        {
+            SetActive(mainCommandButtons, true);
+            SetActive(skillListPanel, false);
+            SetActive(swapListPanel, false);
+            SetActive(itemListPanel, true);
+        }
+
+        private void BindButtons()
+        {
+            if (fightButton != null)
             {
-                Button btn = Instantiate(simpleButtonPrefab, sideListRoot);
-                btn.GetComponentInChildren<TMP_Text>().text = $"{reserve.Name} HP:{reserve.CurrentHP} MP:{reserve.CurrentMP}";
-                btn.onClick.AddListener(() => OnReserveClicked?.Invoke(reserve));
+                fightButton.onClick.RemoveListener(ShowSkills);
+                fightButton.onClick.AddListener(ShowSkills);
             }
-            descriptionText.text = "控えを選択して即交代（行動権消費なし）";
+
+            if (swapButton != null)
+            {
+                swapButton.onClick.RemoveListener(ShowSwap);
+                swapButton.onClick.AddListener(ShowSwap);
+            }
+
+            if (itemButton != null)
+            {
+                itemButton.onClick.RemoveListener(ShowItems);
+                itemButton.onClick.AddListener(ShowItems);
+            }
         }
 
-        private void OpenItems()
+        private void UnbindButtons()
         {
-            _mode = CommandViewMode.Items;
-            ClearChildren(skillListRoot);
-            ClearChildren(sideListRoot);
-
-            Button btn = Instantiate(simpleButtonPrefab, sideListRoot);
-            btn.GetComponentInChildren<TMP_Text>().text = "Potion (仮)";
-            btn.onClick.AddListener(() => OnItemClicked?.Invoke("Potion"));
-            descriptionText.text = "前方マスの味方に回復アイテムを使用";
+            if (fightButton != null) fightButton.onClick.RemoveListener(ShowSkills);
+            if (swapButton != null) swapButton.onClick.RemoveListener(ShowSwap);
+            if (itemButton != null) itemButton.onClick.RemoveListener(ShowItems);
         }
 
-        public void SetInteractable(bool interactable)
+        private static void SetActive(GameObject target, bool active)
         {
-            fightButton.interactable = interactable;
-            swapButton.interactable = interactable;
-            itemButton.interactable = interactable;
-            foreach (Button btn in skillListRoot.GetComponentsInChildren<Button>()) btn.interactable = interactable;
-            foreach (Button btn in sideListRoot.GetComponentsInChildren<Button>()) btn.interactable = interactable;
-        }
-
-        private static void ClearChildren(Transform root)
-        {
-            for (int i = root.childCount - 1; i >= 0; i--) Destroy(root.GetChild(i).gameObject);
-        }
-
-        private static void AddHoverEvent(EventTrigger trigger, EventTriggerType type, Action callback)
-        {
-            var entry = new EventTrigger.Entry { eventID = type };
-            entry.callback.AddListener(_ => callback());
-            trigger.triggers.Add(entry);
+            if (target != null)
+            {
+                target.SetActive(active);
+            }
         }
     }
 }
