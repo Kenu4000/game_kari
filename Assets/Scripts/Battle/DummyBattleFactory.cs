2f624cb200e56c47ac3506fe970f5080ef953909
Kenu4000 <komabuchi4000@gmail.com>
2026-05-20 03:29:05 +0900

    Remove legacy runtime character creation

--- Assets/Scripts/Battle/DummyBattleFactory.cs
+++ Assets/Scripts/Battle/DummyBattleFactory.cs
@@ -15,16 +15,5 @@
             CharacterData data = DummyCharacterFactory.CreateCharacterDataById(characterId);
             return new BattleUnit(data);
         }
-
-        public static BattleUnit CreateEnemyUnit(string name, int hp, int speed)
-        {
-            return CreateBaseUnit(name, hp, speed);
-        }
-
-        private static BattleUnit CreateBaseUnit(string name, int hp, int speed)
-        {
-            CharacterData data = DummyCharacterFactory.CreateCharacterData(name, hp, speed);
-            return new BattleUnit(data);
-        }
     }
 }
--- Assets/Scripts/Battle/DummyCharacterFactory.cs
+++ Assets/Scripts/Battle/DummyCharacterFactory.cs
@@ -5,7 +5,6 @@ namespace GameKari.Battle
 {
     public static class DummyCharacterFactory
     {
-        private const int DefaultMaxMP = 4;
         private const string CharacterAssetBasePath = "Battle/Characters/";
 
         public static CharacterData CreateCharacterDataById(string characterId)
@@ -19,33 +18,6 @@ namespace GameKari.Battle
             throw new InvalidOperationException($"CharacterData asset not found for id: {characterId}");
         }
 
-        public static CharacterData CreateCharacterData(string name, int hp, int speed)
-        {
-            string characterId = BuildCharacterId(name);
-            CharacterData asset = LoadCharacterAsset(characterId);
-            if (asset != null)
-            {
-                return asset;
-            }
-
-            return CreateRuntimeCharacterData(characterId, name, hp, speed);
-        }
-
-        private static CharacterData CreateRuntimeCharacterData(string characterId, string displayName, int hp, int speed)
-        {
-            CharacterData data = ScriptableObject.CreateInstance<CharacterData>();
-
-            data.Id = characterId;
-            data.DisplayName = displayName;
-            data.MaxHP = hp;
-            data.MaxMP = DefaultMaxMP;
-            data.Speed = speed;
-
-            AssignDefaultSkills(data);
-
-            return data;
-        }
-
         private static CharacterData LoadCharacterAsset(string characterId)
         {
             if (string.IsNullOrEmpty(characterId))
@@ -54,57 +26,6 @@ namespace GameKari.Battle
             }
 
             return Resources.Load<CharacterData>(CharacterAssetBasePath + characterId);
-        }
-
-        private static void AssignDefaultSkills(CharacterData data)
-        {
-            if (data == null)
-            {
-                return;
-            }
-
-            data.DefaultSkills.Clear();
-
-            switch (data.Id)
-            {
-                case "knight":
-                    data.DefaultSkills.Add(DummySkillCatalog.GetSlash());
-                    data.DefaultSkills.Add(DummySkillCatalog.GetPierce());
-                    data.DefaultSkills.Add(DummySkillCatalog.GetTwinHit());
-                    data.DefaultSkills.Add(DummySkillCatalog.GetFocus());
-                    break;
-
-                case "mage":
-                    data.DefaultSkills.Add(DummySkillCatalog.GetSlash());
-                    data.DefaultSkills.Add(DummySkillCatalog.GetPierce());
-                    data.DefaultSkills.Add(DummySkillCatalog.GetFocus());
-                    break;
-
-                case "cleric":
-                    data.DefaultSkills.Add(DummySkillCatalog.GetSlash());
-                    data.DefaultSkills.Add(DummySkillCatalog.GetFocus());
-                    break;
-
-                case "rogue":
-                    data.DefaultSkills.Add(DummySkillCatalog.GetSlash());
-                    data.DefaultSkills.Add(DummySkillCatalog.GetPierce());
-                    data.DefaultSkills.Add(DummySkillCatalog.GetFocus());
-                    break;
-
-                default:
-                    data.DefaultSkills.Add(DummySkillCatalog.GetSlash());
-                    break;
-            }
-        }
-
-        private static string BuildCharacterId(string name)
-        {
-            if (string.IsNullOrEmpty(name))
-            {
-                return "";
-            }
-
-            return name.ToLower().Replace(" ", "_");
         }
     }
 }
