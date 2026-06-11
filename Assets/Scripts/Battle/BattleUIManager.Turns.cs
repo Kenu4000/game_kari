6ea8f9eebc1081211fd9bcd1cdd1ab2eeb9ca6c4
Kenu4000 <komabuchi4000@gmail.com>
2026-06-11 12:03:41 +0900

    Split BattleUIManager methods into readable partial files

--- Assets/Scripts/Battle/BattleUIManager.Turns.cs
+++ Assets/Scripts/Battle/BattleUIManager.Turns.cs
@@ -1,3 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 namespace GameKari.Battle
 {
     public partial class BattleUIManager
